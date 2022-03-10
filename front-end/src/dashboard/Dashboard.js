3480f9cd1a01990062330a05f4242c83b1722bdf
SobranDM <sobran@gmail.com>
2022-03-10 14:12:45 -0800

    fixed dashboard

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import { today } from "../utils/date-time";
+import { today, previous, next } from "../utils/date-time";
 
 /**
  * Defines the dashboard page.
@@ -15,6 +15,7 @@ function Dashboard() {
   const [reservationsError, setReservationsError] = useState(null);
   const search = useLocation().search;
   const dateParam = new URLSearchParams(search).get('date');
+
   let date;
   if (dateParam) {
     date = dateParam;
@@ -22,6 +23,8 @@ function Dashboard() {
     date = today();
   }
 
+  let previousDay = previous(date);
+  let nextDay = next(date);
 
   useEffect(() => loadDashboard(date), [date]);
 
@@ -36,9 +39,16 @@ function Dashboard() {
 
   return (
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <div className="d-md-flex mb-3 flex-column align-items-center">
+        <h1>Dashboard</h1>
+        <div className="d-flex flex-row mb-3 mt-4">
+          <h4 className="mr-3">Reservations for {date}</h4>
+          <div className="btn-group" role="group" aria-label="navigate dates">
+            <Link to={"/dashboard?date=" + previousDay}><button type="button" className="btn btn-secondary">Prev</button></Link>
+            <Link to={"/dashboard"}><button type="button" className="btn btn-secondary">Today</button></Link>
+            <Link to={"/dashboard?date=" + nextDay}><button type="button" className="btn btn-secondary">Next</button></Link>
+          </div>
+        </div>
         <table className="table">
           <thead>
             <tr>
