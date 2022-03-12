b811a84ffbc491c88c6aa99a18b63bc9c8d155ac
SobranDM <sobran@gmail.com>
2022-03-11 21:40:37 -0800

    setup create table frontend

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { CreateReservation } from "../reservations/CreateReservation";
+import { CreateTable } from "../tables/CreateTable";
 
 /**
  * Defines all the routes for the application.
@@ -23,6 +24,9 @@ function Routes() {
       </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/tables/new">
+        <CreateTable />
       </Route>
       <Route path="/dashboard">
         <Dashboard />
---
+++ front-end/src/tables/CreateTable.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import { TableForm } from './TableForm';
+import { createTable } from "../utils/api.js";
+
+export const CreateTable = () => {
+  const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [formData, setFormData] = useState({ table_name: "", capacity: 1 });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      let parsedForm = { ...formData, capacity: Number(formData.capacity) }
+      await createTable(parsedForm).then(() => {
+        history.push("/dashboard");
+      })
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+
+  function handleCancel() {
+    history.goBack();
+  }
+
+  return (
+    <div>
+      <h1 className="my-3">Create Table</h1>
+      {errorMessage && (
+        <h5 className="alert alert-danger mx-1">{errorMessage}</h5>
+      )}
+      <TableForm formData={formData} setFormData={setFormData} />
+      <div className="btn-group ml-1 mt-1">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          name="cancel"
+          onClick={(event) => handleCancel()}>
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          name="submit"
+          onClick={(event) => handleSubmit(event)}>
+          Submit
+        </button>
+      </div>
+    </div>
+  )
+}
