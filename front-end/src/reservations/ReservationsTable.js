b91972b6c595183612aadadafa16be6fd5ef882a
SobranDM <sobran@gmail.com>
2022-03-25 01:31:41 -0700

    changed reservation table layout

--- front-end/src/reservations/ReservationsTable.js
+++ front-end/src/reservations/ReservationsTable.js
@@ -26,8 +26,7 @@ export const ReservationsTable = ({ reservations, tableChange, triggerTableChang
         <thead className="table-secondary">
           <tr>
             <th scope="col">Time</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
+            <th scope="col">Name</th>
             <th scope="col">People</th>
             <th scope="col">Mobile Number</th>
             <th scope="col">Status</th>
@@ -39,8 +38,7 @@ export const ReservationsTable = ({ reservations, tableChange, triggerTableChang
             return (
               <tr key={reservation.reservation_id}>
                 <td>{reservation.reservation_time}</td>
-                <td>{reservation.first_name}</td>
-                <td>{reservation.last_name}</td>
+                <td>{reservation.first_name} {reservation.last_name}</td>
                 <td>{reservation.people}</td>
                 <td>{reservation.mobile_number}</td>
                 <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
